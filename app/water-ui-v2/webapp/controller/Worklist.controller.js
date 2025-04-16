369514bc584b25ce23d6000d3f1b33ad219f6a94
Harry Elliott <harry.elliott@delaware.co.uk>
2025-04-16 15:23:38 +0000

    Working Object page (no data yet)

--- app/water-ui-v2/webapp/controller/Worklist.controller.js
+++ app/water-ui-v2/webapp/controller/Worklist.controller.js
@@ -7,15 +7,49 @@ sap.ui.define([
         onInit() {
         },
         
+		/**
+		 * Shows the selected item on the object page
+		 * On phones a additional history entry is created
+		 * @param {sap.m.ObjectListItem} oItem selected Item
+		 * @private
+		 */
+		_showObject : function (oItem) {
+			this.getOwnerComponent().getRouter().navTo("object", {
+				objectId: oItem.getBindingContext().getProperty("ProductID")
+			});
+			console.log("show object section" + oItem);
+		},
+
         /**
 		 * Event handler when a table item gets pressed
 		 * @param {sap.ui.base.Event} oEvent the table selectionChange event
 		 * @public
 		 */
 		onPress : function (oEvent) {
+
+			var oItem = oEvent.getSource();
+			
+			var sPath = oItem.getBindingContext().getPath();
+			var sProductID = sPath.match(/'([^']+)'/)[1];
+
+			// var sProductID = oItem.getBindingContext().getProperty("ProductID");
+
+			console.log(sPath);
+			console.log(sProductID);
+			console.log(oItem.getBindingContext());
+
+
+			 this.getOwnerComponent().getRouter().navTo("Object", {
+			 	objectId: sProductID
+			 })
+
 			// The source is the list item that got pressed
-			this._showObject(oEvent.getSource());
+			// this._showObject(oEvent.getSource());
 		},
+		
+		
         
     });
 });
+
+	

fe179a8408ed221cd8ccd9a4003c04fd88b071ee
Harry Elliott <harry.elliott@delaware.co.uk>
2025-05-16 08:03:27 +0000

    Styling and formatting of the object page

--- app/water-ui-v2/webapp/model/formatter.js
+++ app/water-ui-v2/webapp/model/formatter.js
@@ -1,28 +1,37 @@
 sap.ui.define([
 
-], 
+],
 
-function () {
-    "use strict";
+    function () {
+        "use strict";
 
-    return {
-        /**
-        * @public
-		* @param {string} mpriority the number to be converted for the priority of the maintenance
-        */
-        priorityMethod: function(mpriority) {
-            console.log("Formatter function is being run")
-            switch(mpriority){
-                case 1:
-                    return "Low";
-                case "2":
-                    return "Medium";
-                case 3:
-                    return "High";
-                default:
-                    return "Unknown";
-                    
+        return {
+            /**
+            * @public
+            * @param {string} mpriority the number to be converted for the priority of the maintenance
+            */
+            priorityMethod: function (mpriority) {
+                switch (mpriority) {
+                    case "1":
+                        return "High";
+                    case "2":
+                        return "Medium";
+                    case "3":
+                        return "Low";
+                    default:
+                        return "Unknown";
+                }
+            },
+
+            /**
+            * @public
+            * @param {Date} mDate date
+            */
+            date: function (mDate) {
+                if (mDate) {
+                    return new Date(mDate).toLocaleDateString('en-GB');
+                }
+                return "";
             }
-        }
-    };
-});
+        };
+    });
